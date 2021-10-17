36b857d1c71518c31275c3b8f358dff3f524df62
Mbarov <prom@tgv73.ru>
2021-10-17 13:09:49 +0400

    Diploy

--- resume/settings.py
+++ resume/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
-import dj-database-url
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -132,5 +132,3 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-db_from_env = dj-database-url.config()
-DATABASE['default'].update(db_from_env)
